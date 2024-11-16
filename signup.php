0e7b0071dd0193aa9f74c1a34d4379440da3c24a
adibmahmud007 <adibmahmud307@gmail.com>
2024-11-17 00:10:44 +0600

    signup page design

--- signup.php
+++ signup.php
@@ -10,64 +10,21 @@
 
 <body>
 
-  <Section>
-    <div class="w-[500px] bg-slate-300 flex flex-col justify-center items-center">
-      <form class=" w-[400px]" action="signup.php" method="post">
-        <label class="input input-bordered flex items-center gap-2">
-          <input type="text" class="grow" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              fill-rule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clip-rule="evenodd" />
-          </svg>
-        </label>
-        <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-            <path
-              d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-          </svg>
-          <input type="text" class="grow" placeholder="Email" />
-        </label>
-        <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-          </svg>
-          <input type="text" class="grow" placeholder="Username" />
-        </label>
-        <label class="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            class="h-4 w-4 opacity-70">
-            <path
-              fill-rule="evenodd"
-              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-              clip-rule="evenodd" />
-          </svg>
-          <input type="password" class="grow" value="password" />
-        </label>
-      </form>
-    </div>
-    <div>
+  <Section class="bg-slate-200 w-full min-h-screen flex justify-center items-center">
+    <div class="w-[900px] h-[450px] bg-white rounded-xl  shadow-xl flex gap-12 pt-20 pl-14">
 
+      <form class="flex gap-10 flex-col  w-[400px]" action="signup.php" method="post">
+        <input class="border border-black py-2 px-3 rounded-lg text-xl font-semibold" type="text" name="username" placeholder="Enter Username">
+        <input class="border border-black py-2 px-3 rounded-lg text-xl font-semibold" type="email" name="email" placeholder="Enter Email">
+        <input class="border border-black py-2 px-3 rounded-lg text-xl font-semibold" type="password" name="password" placeholder="Enter Password">
+        <input class="px-2 py-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 w-[100px] rounded-lg text-lg font-semibold" type="submit" name="signup" value="Signup">
+      </form>
+      <div class="mt-12 ">
+        <h2 class="text-3xl font-bold ml-5">Signin to PageSwap</h2>
+        <p class="text-xl font-semibold italic ml-4">Get to the world of knowledge <br> by sharing books</p>
+      </div>
     </div>
+
   </Section>
 </body>
 

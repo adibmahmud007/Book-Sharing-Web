cfbf66f40bc6102469b0f59d2e2d9cf518135b4f
adibmahmud007 <adibmahmud307@gmail.com>
2024-11-20 00:49:18 +0600

    Signup page design completed

--- signup.php
+++ signup.php
@@ -14,21 +14,21 @@
     <img src="./assets/Page Swap.png" class="w-full h-fit  relative translate-y-[-80px] " alt="Dining Experience" />
 
     <div class="min-h-screen flex fle-col items-center justify-center px-10 absolute">
-      <div class="grid lg:grid-cols-2 pl-12 mt-6 items-center gap-6 max-w-7xl max-lg:max-w-xl w-full">
+      <div class="grid lg:grid-cols-2 pl-12 mt-3 items-center gap-6 max-w-7xl max-lg:max-w-xl w-full">
         <form class="lg:max-w-md w-full">
-          <h3 class="text-gray-800 text-3xl font-extrabold mb-6">Registration</h3>
-          <div class="space-y-2">
+          <h3 class="text-gray-800 text-5xl font-extrabold mb-6">Registration</h3>
+          <div class="space-y-6">
             <div>
-              <label class="text-gray-800 text-sm mb-2 block">Name</label>
-              <input name="name" type="text" class="bg-gray-100 w-[380px] text-gray-800 px-4 py-3 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter name" />
+              <label class="text-gray-800 text-lg mb-2 block">Name</label>
+              <input name="name" type="text" class="bg-gray-100 w-[480px] text-gray-800 px-5 py-4 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter name" />
             </div>
             <div>
-              <label class="text-gray-800 text-sm mb-2 block">Email</label>
-              <input name="email" type="text" class="bg-gray-100 w-full text-gray-900  px-4 py-3 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter email" />
+              <label class="text-gray-800 text-lg mb-2 block">Email</label>
+              <input name="email" type="text" class="bg-gray-100 w-[480px] text-gray-900  px-5 py-4 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter email" />
             </div>
             <div>
-              <label class="text-gray-800 text-sm mb-2 block">Password</label>
-              <input name="password" type="password" class="bg-gray-100 w-full text-gray-800  px-4 py-3 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter password" />
+              <label class="text-gray-800 text-lg mb-2 block">Password</label>
+              <input name="password" type="password" class="bg-gray-100 w-[480px] text-gray-800  px-5 py-4 rounded-lg focus:bg-transparent outline-black transition-all text-lg font-semibold" placeholder="Enter password" />
             </div>
             <!-- <div class="flex items-center">
               <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 shrink-0 border-gray-300 rounded" />
@@ -39,11 +39,11 @@
           </div>
 
           <div class="mt-8">
-            <button type="button" class="py-4 px-8 text-sm font-semibold rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+            <button type="button" class="py-4 px-8 text-md font-semibold rounded-md transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
               Create an account
             </button>
           </div>
-          <p class="text-md text-gray-800 mt-6">Already have an account? <a href="javascript:void(0);" class="text-green-700 font-bold hover:cursor-pointer ml-1">Login here</a></p>
+          <p class="text-lg text-gray-800 mt-8">Already have an account? <a href="javascript:void(0);" class="text-green-700 font-bold hover:cursor-pointer ml-1">Login here</a></p>
         </form>
 
       </div>

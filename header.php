bef17c0f1f536b66ccf5d8b0d4b54d3ecac4f0cf
adibmahmud007 <adibmahmud307@gmail.com>
2024-11-30 22:26:17 +0600

    header elements modified

--- header.php
+++ header.php
@@ -56,14 +56,14 @@
                             <a href='javascript:void(0)'
                                 class='lg:hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]'>Home</a>
                         </li>
-                        <li class='max-lg:border-b max-lg:py-3 px-3'><a href='javascript:void(0)'
-                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Team</a>
+                        <li class='max-lg:border-b max-lg:py-3 px-3'><a href='profile.php'
+                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Profile</a>
                         </li>
                         <li class='max-lg:border-b max-lg:py-3 px-3'><a href='javascript:void(0)'
-                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Feature</a>
+                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Post Books</a>
                         </li>
                         <li class='max-lg:border-b max-lg:py-3 px-3'><a href='javascript:void(0)'
-                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Blog</a>
+                                class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Book Requests</a>
                         </li>
                         <li class='max-lg:border-b max-lg:py-3 px-3'><a href='javascript:void(0)'
                                 class='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>About</a>
